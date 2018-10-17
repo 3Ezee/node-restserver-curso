b1d6794a79d81a5b15d2407a27bd14ea41c959df
Ezequiel <ezequielsobrino@gmail.com>
2018-10-17 19:55:47 -0300

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,20 @@
 // Puerto
 //================
 process.env.PORT = process.env.PORT || 3000;
+
+//================
+// Entorno
+//================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//================
+// Base de datos
+//================
+let urlDB
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe-user:123456a@ds163769.mlab.com:63769/cafe';
+}
+process.env.urlDB = urlDB;
