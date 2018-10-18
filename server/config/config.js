342c15de958de528b59d6a3b7888f9dae6e6d23a
Ezequiel <ezequielsobrino@gmail.com>
2018-10-18 19:05:34 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -11,11 +11,13 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 //=================
 // Base de datos
 //=================
-let urlDB
+let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
     urlDB = process.env.MONGO_URI;
 }
-process.env.urlDB = urlDB;
+
+
+process.env.URLDB = urlDB;
