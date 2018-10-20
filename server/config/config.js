aa983040b35513917a94bcf45a36a04e009f7ab3
Ezequiel <ezequielsobrino@gmail.com>
2018-10-19 23:50:04 -0300

    Login normal

--- server/config/config.js
+++ server/config/config.js
@@ -9,6 +9,15 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 //=================
+// Vencimiento del Token
+//=================
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+//=================
+// Seed de autenticación
+//=================
+process.env.SEED = process.env.SEED || 'secret';
+//=================
 // Base de datos
 //=================
 let urlDB;
