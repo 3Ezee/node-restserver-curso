63ab9822a5bc5fc155a92fab7a22d497ab009cdf
Ezequiel <ezequielsobrino@gmail.com>
2018-10-22 00:57:38 -0300

    Google Sign-in

--- server/config/config.js
+++ server/config/config.js
@@ -29,3 +29,8 @@ if (process.env.NODE_ENV === 'dev') {
 }
 
 process.env.URLDB = urlDB;
+
+//=================
+// Google client ID
+//=================
+process.env.CLIENT_ID = process.env.CLIENT_ID || '910602292662-le60sgou4g01fbhkrf1q4iek7vh55bmu.apps.googleusercontent.com'
